49850c0fc77d7b7fbc4e19522254a494ae34ba3e
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2013-12-04 15:40:59 +0100

    FIX TravisCI build containers

--- travis.sh
+++ travis.sh
@@ -23,31 +23,25 @@ echo "--- Common Lisp container ---"
 cd $PORTEFAIX_HOME
 docker build -t nlamirault/commonlisp commonlisp
 #docker run -i -t nlamirault/commonlisp /usr/bin/sbcl --eval "(format t \"Hello Portefaix\")" --quit --non-interactive
-#cd $PORTEFAIX_HOME
 
 echo "--- Erlang container ---"
 cd $PORTEFAIX_HOME
-docker build -t nlamirault/erlang erlang
-#cd $PORTEFAIX_HOME
+#docker build -t nlamirault/erlang erlang
 
 echo "--- GO container ---"
 cd $PORTEFAIX_HOME/go
 docker build -t nlamirault/go .
-#cd $PORTEFAIX_HOME
 
 echo "--- Haskell container ---"
 cd $PORTEFAIX_HOME/haskell
-docker build -t nlamirault/haskell .
-#cd $PORTEFAIX_HOME
+#docker build -t nlamirault/haskell .
 
 echo "--- OCaml container ---"
 cd $PORTEFAIX_HOME/ocaml
-docker build -t nlamirault/ocaml .
-#cd $PORTEFAIX_HOME
+#docker build -t nlamirault/ocaml .
 
 echo "--- Python container ---"
 cd $PORTEFAIX_HOME/python
-docker build -t nlamirault/python .
-#cd $PORTEFAIX_HOME
+#docker build -t nlamirault/python .
 
 echo "--- Portefaix containers build done. ---"
