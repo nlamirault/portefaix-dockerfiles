a3c0dc42b4d439a891e76ce3cca23970559c5c57
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2013-12-04 15:09:55 +0100

    FIX current directory

--- travis.sh
+++ travis.sh
@@ -3,14 +3,15 @@
 # Exit on first error
 set -e
 
+PORTEFAIX_HOME=$(dirname "$0")
+echo $PORTEFAIX_HOME
+
 # Kill background processes on exit
 trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
 
 # Start docker daemon
 docker -d &
 sleep 1
-
-PORTEFAIX_HOME=`$(dirname "$0")`
 
 echo "---------------------"
 echo "----- Portefaix -----"
